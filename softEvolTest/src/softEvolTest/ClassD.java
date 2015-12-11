2c8fc06673d9eae543a1b3a2e7aaa3c22ed83b61
Spiros Tzavaras <spirostza77@hotmail.com>
2015-12-11 02:00:10 +0100

    not fixed . . .

--- softEvolTest/src/softEvolTest/ClassD.java
+++ softEvolTest/src/softEvolTest/ClassD.java
@@ -2,24 +2,24 @@ package softEvolTest;
 
 
 
-//public class ClassD {
-//	private int a;
-//	private int b;
-//	private int d;
-//	
-//	public int sumInClassD(int a, int d) {
-//		this.a = a;
-//		this.d = d;				
-//		System.out.println(this.a);
-//		System.out.println(this.a + "+" + this.d + "=");		
-//		return this.a + this.d;
-//	}
-//	
-//	public int sumInClassDD(int a, int d) {
-//		this.a = a;
-//		this.d = d;				
-//		System.out.println(this.a);
-//		System.out.println(this.a + "+" + this.d + "=");		
-//		return this.a + this.d;
-//	}
-//}
+public class ClassD {
+	private int a;
+	private int b;
+	private int d;
+	
+	public int sumInClassD(int a, int d) {
+		this.a = a;
+		this.d = d;				
+		System.out.println(this.a);
+		System.out.println(this.a + "+" + this.d + "=");		
+		return this.a + this.d;
+	}
+	
+	public int sumInClassDD(int a, int d) {
+		this.a = a;
+		this.d = d;				
+		System.out.println(this.a);
+		System.out.println(this.a + "+" + this.d + "=");		
+		return this.a + this.d;
+	}
+}
